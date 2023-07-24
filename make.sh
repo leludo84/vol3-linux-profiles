bf0ea9211159d5a65c66aefbac73849b9be378bc
ludwigvonunix <ludwigvonunix.noreply.com>
2023-07-24 14:54:31 +0000

    Add centos8 processor

--- make.sh
+++ make.sh
@@ -2,6 +2,7 @@
 set -e -x
 
 cd sources
+docker build ./ -f centos8/Dockerfile -t profilator-centos8
 docker build ./ -f centos7/Dockerfile -t profilator-centos7
 docker build ./ -f centos6/Dockerfile -t profilator-centos6
 docker build ./ -f almalinux8/Dockerfile -t profilator-almalinux8
