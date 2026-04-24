47e855f71c78f5d324f6bce25748538b4ff39d24
leludo84 <leludo84@volatility-profiles.lab.local>
2026-04-24 11:54:34 +0200

    Banners update

---
+++ run-current.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+update_global_banners=0
+for distrib in centos8 almalinux8 almalinux9 almalinux10 ubuntu16 ubuntu18 ubuntu20 ubuntu22 ubuntu24 debian-snapshot
+do
+	docker run -it --rm -v ./profiles/$distrib:/root/profiles profilator-$distrib
+	git pull
+	git diff --exit-code ./profiles/$distrib || update_global_banners=1
+	git add ./profiles/$distrib
+	git commit -m"Add new $distrib vol3 profiles batch." ./profiles/$distrib
+	git push
+done
+if [ "$update_global_banners" = "1" ]
+then
+	echo "################################"
+	echo " Update global banners files"
+	echo "################################"
+	./make-banners.sh profiles/
+	./bannersToISF.py
+	git commit -m"Banners updates" banners.ndjson banners-isf.json
+	git push
+fi
+
+date
+
